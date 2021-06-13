1f2bffcc29e5ec4dab3432c6a185c363051ee810
Leandro <fahurleandro@gmail.com>
2021-06-12 18:07:25 -0700

    middleware

--- server/src/shared/http/server.ts
+++ server/src/shared/http/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import checkServer from '../middleware/checkServer';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(checkServer);
 app.use(routes);
 
 app.listen('5000', () => {
