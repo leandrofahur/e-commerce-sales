d8baeab715d16a11869ff82d4ea5111612ebc2b5
Leandro <fahurleandro@gmail.com>
2021-06-12 17:46:08 -0700

    boilerplate for express

--- server/src/shared/http/server.ts
+++ server/src/shared/http/server.ts
@@ -1,1 +1,13 @@
-console.log('Teste de sanidade...');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen('5000', () => {
+  console.log('⚡︎ Server up and running on port 5000');
+});
