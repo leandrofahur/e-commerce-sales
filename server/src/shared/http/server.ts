4e4b7f14f86bb707d2aaf785ba53b70e059dc94a
Leandro <fahurleandro@gmail.com>
2021-07-20 11:03:16 -0700

    celebrate as middleware for checking

--- server/src/shared/http/server.ts
+++ server/src/shared/http/server.ts
@@ -1,16 +1,19 @@
 import 'reflect-metadata';
 import express from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import checkServer from '../middleware/checkServer';
+import { errors } from 'celebrate';
 import '@shared/typeorm';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(checkServer);
 app.use(routes);
+app.use(errors());
+app.use(checkServer);
 
 app.listen('5000', () => {
   console.log('⚡︎ Server up and running on port 5000');
