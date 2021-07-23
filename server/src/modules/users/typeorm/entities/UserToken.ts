4ceb547c81c43faecc5a28eb84862581a525f018
Leandro <fahurleandro@gmail.com>
2021-07-23 08:44:49 -0700

    user token entity + repository

---
+++ server/src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+  Generated,
+} from 'typeorm';
+
+import { v4 as uuid } from 'uuid';
+
+@Entity('user_tokens')
+export class UserToken extends BaseEntity {
+  @PrimaryColumn('uuid')
+  readonly id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    super();
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
